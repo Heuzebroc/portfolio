b0f32b8a3dadb14c685fc607bd1e087900e8698f
Barack Obama <hugo.heuzebroc.oise@gmail.com>
2022-01-31 17:37:03 +0100

    adding typing animation

--- assets/js/typing-animation.js
+++ assets/js/typing-animation.js
@@ -1,50 +1,73 @@
-const title = document.querySelector("header h1");
-const titleText = title.innerHTML;
-const subtitle = document.querySelector("header h2");
-const subtitleText = subtitle.innerHTML;
+//elements needed
+const title = document.querySelector("header h1"),
+    titleText = title.innerHTML;
+const subtitle = document.querySelector("header h2"),
+    subtitleText = subtitle.innerHTML;
+const mainSection = document.querySelector('#main');
 
-let element = title, text = titleText, currentFontSize = 1;
-
-//emptying the titles for now. There has to be some text inside for them to keep the same height
+//preparing the document
 title.innerHTML = "";
 subtitle.innerHTML = "";
-subtitle.style.fontSize = `${currentFontSize}rem`;
+subtitle.style.fontSize = "50%";
+mainSection.style.display = "none";
 
 //typing one character
-const typeOne = () => {
-    //if(element.innerHTML === "&nbsp;") element.innerHTML = "";
-
+const typeOne = (element, text, delay) => {
     element.innerHTML += text.charAt(element.innerHTML.length);
 
-    if(element.innerHTML != text) setTimeout(typeOne, 60);
-    else if(element === title) {
-        element = subtitle;
-        text = subtitleText;
+    if(element.innerHTML !== text) setTimeout(() => typeOne(element, text, delay), delay);
+    else nextStep();
+};
 
-        setTimeout(typeOne, 400);
+//highlighting an element
+const highlight = (element, doHighlight) => {
+    //inline element
+    if(element.style.display !== "block" && element.style.display !== "none") {
+        if(doHighlight) element.innerHTML = `<mark>${element.innerHTML}</mark>`
+        else element.innerHTML = element.innerHTML.substr(6, element.innerHTML.length-13);
     }
+
+    //block element
     else {
-        setTimeout(highlight, 500);
+        if(doHighlight) {
+            element.innerHTML = element.innerHTML
+                .replaceAll("<p>", "<p><mark>")
+                .replaceAll("</p>", "</mark></p>");
+        }
+        else {
+            element.innerHTML = element.innerHTML
+                .replaceAll("<p><mark>", "<p>")
+                .replaceAll("</mark></p>", "</p>");
+        }
     }
+
+    nextStep();
 };
 
-//highlighting the subtitle
-const highlight = () => {
-    subtitle.classList.toggle("highlighted");
-
-    if(subtitle.classList.contains("highlighted")) setTimeout(resizeOnce, 200);
-};
-
-//resizing
-const resizeOnce = () => {
-    currentFontSize += 0.25;
-    subtitle.style.fontSize = `${currentFontSize}rem`;
-
-    if(currentFontSize<2) setTimeout(resizeOnce, 200);
-    else setTimeout(highlight, 200);
+//changing a CSS property
+const css = (element, property, value) => {
+    element.style[property] = value;
+    nextStep();
 }
 
-//pasting main text
+//MAIN SEQUENCE
+const steps = [
+    {step: () => typeOne(title, titleText, 70), delay: 500},
+    {step: () => typeOne(subtitle, subtitleText, 50), delay: 150},
+    {step: () => highlight(subtitle, true), delay: 150},
+    {step: () => css(subtitle, "font-size", "75%"), delay: 150},
+    {step: () => css(subtitle, "font-size", "100%"), delay: 150},
+    {step: () => highlight(subtitle, false), delay: 150},
+    {step: () => highlight(mainSection, true), delay: 0},
+    {step: () => css(mainSection, "display", "block"), delay: 150},
+    {step: () => highlight(mainSection, false), delay: 250},
+];
+let currentStep = 0;
 
+const nextStep = () => {
+    if(currentStep < steps.length) setTimeout(steps[currentStep].step, steps[currentStep].delay);
 
-setTimeout(typeOne, 800);
+    currentStep++;
+}
+
+nextStep();
