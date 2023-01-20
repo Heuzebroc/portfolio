a1fb979c410d152e3a20af571c005f7cb3f57f50
Barack Obama <hugo.heuzebroc.oise@gmail.com>
2023-01-20 11:45:53 +0100

    navbar

--- assets/js/entrance-animation.js
+++ assets/js/entrance-animation.js
@@ -4,14 +4,14 @@ const toAnimateImmediately = document.querySelectorAll(".entrance-immediate");
 const entrance = element => {
     let delay = 0;
 
-    if(element.classList.contains("entrance-standalone")) {
+    if (element.classList.contains("entrance-standalone")) {
         element.style.transform = "none";
-        element.style.transition = `transform 0.8s ease-out`;
-    }
-
-    else element.querySelectorAll(".entrance > *, .entrance > .entrance-skip > *").forEach(child => {
+        element.style.opacity = "1";
+        element.style.transition = `0.8s ease-out`;
+    } else element.querySelectorAll(".entrance > *, .entrance > .entrance-skip > *").forEach(child => {
         child.style.transform = "none";
-        child.style.transition = `transform 0.8s ease-out ${delay}s`;
+        child.style.opacity = "1";
+        child.style.transition = `0.8s ease-out ${delay}s`;
         delay += 0.2;
     });
 }
@@ -23,9 +23,10 @@ setTimeout(() => toAnimateImmediately.forEach(element => {
 
 let currentScroll = toAnimateImmediately.length;
 
+//revealing elements on scroll
 document.addEventListener("scroll", e => {
     toAnimate.forEach((element, index) => {
-        if(element.getBoundingClientRect().y < 500 && index === currentScroll) {
+        if (element.getBoundingClientRect().y < 500 && index === currentScroll) {
             entrance(element);
             currentScroll++;
         }
---
+++ assets/js/general.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("#contact_form").forEach(form => {
+    form.classList.add("entrance");
+    form.classList.add("entrance-right");
+});
+
+document.querySelector(".nav").style.transform = "none";
